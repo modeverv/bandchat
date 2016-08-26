6cc8343183a122409a2f7910b34ae5b296850c18
modeverv <modeverv@gmail.com>
2016-08-26 15:30:38 +0900

    use prototype

--- js/app.js
+++ js/app.js
@@ -1,76 +1,78 @@
-var App = {
-  load : function(cursor){
-      $.ajax({
-                 async:false,
-                 dataType: "json",
-                 url: "load.php",
-                 data: {cursor:cursor},
-                 success:function(data){
-                     if(data.length > 0){
-                         App._plot(data);
-                     }
-                 }
-             });
-  },
-  refresh : function(){
-    App.load(App._cursor);
-  },
-  say : function(){
-     var content = $("#input").val();
-     if("" == content){
-         return;
-     }
-     var name = getName();
-     var data = {name:name,content:content};
-     $.ajax({
-                async:false,
-                dataType: "json",
-                url : "say.php",
-                data : data,
-                success : function(json){
-                   $("#input").val("");
-                }
-            });
-     App.refresh();
-  },
-  _bottom : function(){
-     setTimeout(function() {
-         window.scroll(0,$(document).height());
-     },0);
-  },
-  _cursor : 0,
-  _data : "",
-  _plot : function(data){
-      var name = getName();
-      for(var i=0,l=data.length;i<l;i++){
-          if(name == data[i].name) {
-              App._plot_(data[i],"me");
-          } else {
-              App._plot_(data[i],"other");
-          }
-          App._cursor = data[i].id;
-      }
-      $("#chat").append(App._data);
-      App._data = "";
-      App._bottom();
-  },
-  _plot_ : function(chat,kind){
-      if(kind == "me"){
+function App(){}
+App.prototype.serverRead = function(_cursor){
+    $.ajax({
+        context:this,
+        async:true,
+        dataType: "json",
+        url: "load.php",
+        data: {cursor:_cursor},
+        success:function(data){
+            if(data.length > 0){
+                this._plot(data);
+            }
+        }
+    });
+};
+App.prototype.refresh = function(){
+    this.serverRead(this._cursor);
+};
+App.prototype.say = function(){
+    var content = $("#input").val();
+    if("" == content){
+        return;
+    }
+    var name = getName();
+    var data = {name:name,content:content};
+    $.ajax({
+        context:this,
+        async:true,
+        dataType: "json",
+        url : "say.php",
+        data : data,
+        success : function(json){
+            $("#input").val("");
+        }
+    });
+    this.refresh();
+};
+/** private */
+App.prototype._cursor = 0;
+App.prototype._data = "";
+App.prototype._bottom = function(){
+    setTimeout(function() {
+        window.scroll(0,$(document).height());
+    },0);
+};
+App.prototype._plot = function(data){
+    var name = getName();
+    for(var i=0,l=data.length;i<l;i++){
+        if(name == data[i].name) {
+            this._plotInner(data[i],"me");
+        } else {
+            this._plotInner(data[i],"other");
+        }
+        this._cursor = data[i].id;
+    }
+    $("#chat").append(this._data);
+    this._data = "";
+    this._bottom();
+};
+App.prototype._plotInner = function(chat,kind){
+    if(kind == "me"){
         var template = $("#me").text();
-      }else{
+    }else{
         var template = $("#other").text();
-      }
-      var plot = template.replace(/\{name\}/,chat.name).replace(/\{content\}/,chat.content).replace(/\{date\}/,chat.created_at);
-      App._data += plot;
-  }
+    }
+    var html = template.replace(/\{name\}/,chat.name).replace(/\{content\}/,chat.content).replace(/\{date\}/,chat.created_at);
+    this._data += html;
 };
 
-
 $(function(){
-  // initial load 
-  App.load("load");
-  // event
-  $("#say").click(function(){ App.say(); });
-  // polling
-  setInterval(App.refresh,1000);
+    var chat = new App();
+    // initial load
+    chat.serverRead("load");
+    // event
+    $("#say").click(function(){ chat.say(); });
+    // polling
+    setInterval(function(){chat.refresh();},5000);
 });
